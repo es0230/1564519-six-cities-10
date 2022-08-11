ffd7a4e62b05adda65da0a51ed1f43379e113707
Aleksandr Molodykh <caneyok@gmail.com>
2022-08-11 13:17:38 +0300

    Внесены мелкие исправления

--- project/src/components/offer-gallery/offer-gallery.tsx
+++ project/src/components/offer-gallery/offer-gallery.tsx
@@ -2,11 +2,13 @@ type OfferGalleryProps = {
   images: string[];
 };
 
+const GALLERY_IMAGE_LIMIT = 6;
+
 function OfferGallery({ images }: OfferGalleryProps): JSX.Element {
   return (
     <div className="property__gallery">
       {images.map((imageSrc, i) => {
-        if (i < 6) {
+        if (i < GALLERY_IMAGE_LIMIT) {
           return (
             <div className="property__image-wrapper" key={imageSrc}>
               <img className="property__image" src={imageSrc} alt="Studio" />
