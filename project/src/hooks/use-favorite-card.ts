6f0f87b0db90ccf71674283c91cc19c1c40b76f0
Aleksandr Molodykh <caneyok@gmail.com>
2022-08-15 22:41:33 +0300

    Реализовано перенапрваление на страницу авторизации при попытке неавторизованного пользователя добавить предложение в избранное

--- project/src/hooks/use-favorite-card.ts
+++ project/src/hooks/use-favorite-card.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useAppDispatch } from '.';
-import { APIRoute } from '../const';
+import { APIRoute, AppRoute } from '../const';
 import { api } from '../store';
 import { toggleIsFavoriteCard } from '../store/app-data/app-data';
 import { Offer } from '../types/offer';
@@ -11,6 +12,7 @@ export const useFavoriteCard = (offer: Offer | undefined): ResultFavoriteCard =>
   const [isCardFavorite, setCardFavorite] = useState(offer?.isFavorite);
   const [sendRequest, setSendRequest] = useState(false);
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (sendRequest) {
@@ -18,7 +20,8 @@ export const useFavoriteCard = (offer: Offer | undefined): ResultFavoriteCard =>
         .then((updatedOffer) => {
           setCardFavorite(updatedOffer.data.isFavorite);
           dispatch(toggleIsFavoriteCard({ id: updatedOffer.data.id, newIsFavorite: updatedOffer.data.isFavorite }));
-        });
+        })
+        .catch(() => navigate(AppRoute.Login));
       setSendRequest(false);
     }
   }, [sendRequest]);
