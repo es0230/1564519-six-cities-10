f427f07764bfc0d66bf326383fad79e9466967e6
Aleksandr Molodykh <caneyok@gmail.com>
2022-08-15 22:58:46 +0300

    Исправлено отображение активного пользователя в хэдере

--- project/src/store/api-actions.ts
+++ project/src/store/api-actions.ts
@@ -1,12 +1,11 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { AxiosInstance } from 'axios';
-import { APIRoute } from '../const'; // , AuthorizationStatus
+import { APIRoute } from '../const';
 import { dropToken, saveToken } from '../services/token';
 import { Offer } from '../types/offer';
 import { AppDispatch, State } from '../types/state';
 import { AuthData } from '../types/auth-data';
 import { UserData } from '../types/user-data';
-//import { loadOffers, setDataLoadedStatus } from './action'; // requireAuthorization, setActiveUser,
 
 export const fetchOffersAction = createAsyncThunk<Offer[], undefined, {
   dispatch: AppDispatch,
@@ -16,9 +15,6 @@ export const fetchOffersAction = createAsyncThunk<Offer[], undefined, {
   'data/fetchOffers',
   async (_arg, { dispatch, extra: api }) => {
     const { data } = await api.get<Offer[]>(APIRoute.Offers);
-    //dispatch(setDataLoadedStatus(true));
-    //dispatch(loadOffers(data));
-    //dispatch(setDataLoadedStatus(false));
     return data;
   },
 );
@@ -43,8 +39,6 @@ export const loginAction = createAsyncThunk<void, AuthData, {
   async ({ login: email, password }, { dispatch, extra: api }) => {
     const { data: { token } } = await api.post<UserData>(APIRoute.Login, { email, password });
     saveToken(token);
-    //dispatch(requireAuthorization(AuthorizationStatus.Auth));
-    //dispatch(setActiveUser(email));
   },
 );
 
@@ -57,7 +51,5 @@ export const logoutAction = createAsyncThunk<void, undefined, {
   async (_arg, { dispatch, extra: api }) => {
     await api.delete(APIRoute.Logout);
     dropToken();
-    //dispatch(requireAuthorization(AuthorizationStatus.NoAuth));
-    //dispatch(setActiveUser(null));
   },
 );
