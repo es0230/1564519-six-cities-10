f740b7df590ad5b81c3ae9246a2da5c4f8b4b96e
Aleksandr Molodykh <caneyok@gmail.com>
2022-08-22 23:05:28 +0300

    Настроено корректное отображение звёзд рейтинга

--- project/src/components/reviews-form/reviews-form.tsx
+++ project/src/components/reviews-form/reviews-form.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { APIRoute } from '../../const';
 import { api } from '../../store';
@@ -6,9 +6,21 @@ import { Comment } from '../../types/comment';
 import { Review } from '../../types/review';
 import { sortReviewsByDate } from '../../util';
 
+const RatingTitles = {
+  perfect: 5,
+  good: 4,
+  notBad: 3,
+  badly: 2,
+  terribly: 1,
+};
+
+const commentMinLength = 50;
+const commentMaxLength = 300;
+const defaultRating = 0;
+
 const initialState: Comment = {
   comment: '',
-  rating: 0,
+  rating: defaultRating,
 };
 
 type ReviewsFormProps = {
@@ -19,6 +31,7 @@ function ReviewsForm({ handleFormSubmit }: ReviewsFormProps): JSX.Element {
   const { id } = useParams();
   const [newComment, setNewComment] = useState<Comment>(initialState);
   const [sendReview, setSendReview] = useState<boolean>(false);
+  const formRef = useRef(null);
 
   useEffect(() => {
     if (sendReview) {
@@ -39,44 +52,89 @@ function ReviewsForm({ handleFormSubmit }: ReviewsFormProps): JSX.Element {
     setNewComment({ ...newComment, comment: value });
   };
 
-  const onFormSubmit = async (evt: React.FormEvent<HTMLButtonElement>) => {
+  const onFormSubmit = (evt: React.FormEvent<HTMLButtonElement>) => {
     evt.preventDefault();
     setSendReview(true);
   };
 
   return (
-    <form className="reviews__form form" action="#" method="post">
+    <form className="reviews__form form" action="#" method="post" ref={formRef}>
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
       <div className="reviews__rating-form form__rating">
-        <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" onClick={ratingClickHandle} />
+        <input
+          className="form__rating-input visually-hidden"
+          name="rating"
+          value="5"
+          id="5-stars"
+          type="radio"
+          onClick={ratingClickHandle}
+          checked={newComment.rating === RatingTitles.perfect}
+          readOnly
+        />
         <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect" >
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" onClick={ratingClickHandle} />
+        <input
+          className="form__rating-input visually-hidden"
+          name="rating"
+          value="4"
+          id="4-stars"
+          type="radio"
+          onClick={ratingClickHandle}
+          checked={newComment.rating === RatingTitles.good}
+          readOnly
+        />
         <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" onClick={ratingClickHandle} />
+        <input
+          className="form__rating-input visually-hidden"
+          name="rating"
+          value="3"
+          id="3-stars"
+          type="radio"
+          onClick={ratingClickHandle}
+          checked={newComment.rating === RatingTitles.notBad}
+          readOnly
+        />
         <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad" >
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" onClick={ratingClickHandle} />
+        <input
+          className="form__rating-input visually-hidden"
+          name="rating"
+          value="2"
+          id="2-stars"
+          type="radio"
+          onClick={ratingClickHandle}
+          checked={newComment.rating === RatingTitles.badly}
+          readOnly
+        />
         <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly" >
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" onClick={ratingClickHandle} />
+        <input
+          className="form__rating-input visually-hidden"
+          name="rating"
+          value="1"
+          id="1-star"
+          type="radio"
+          onClick={ratingClickHandle}
+          checked={newComment.rating === RatingTitles.terribly}
+          readOnly
+        />
         <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly" >
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
@@ -99,7 +157,7 @@ function ReviewsForm({ handleFormSubmit }: ReviewsFormProps): JSX.Element {
         <button
           className="reviews__submit form__submit button"
           type="button"
-          disabled={newComment.comment.length <= 50 || newComment.rating === 0 || newComment.comment.length > 300}
+          disabled={newComment.comment.length <= commentMinLength || newComment.rating === defaultRating || newComment.comment.length > commentMaxLength}
           onClick={onFormSubmit}
         >
           Submit
